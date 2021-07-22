718dbff24eb8a3fefe4fed2ae38477cd603eba48
exghost <robertlucas198@gmail.com>
2021-07-22 18:07:49 -0400

    add eorzea clock

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 import './Header.css';
 
+import EorzeaClock from 'components/EorzeaClock';
+
 function Header() {
     return (
         <nav className="header">
+            <div className="header__clock-container">
+                <EorzeaClock />
+            </div>
             <h1 className="heading header__heading">
                 FFXIV Sightseeing Log Tracker
             </h1>

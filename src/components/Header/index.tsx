cbc6813d8a4bd44749f8ab23c4ff1d40d147e9a2
exghost <robertlucas198@gmail.com>
2021-07-22 17:40:01 -0400

    add Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Header.css';
+
+function Header() {
+    return (
+        <nav className="header">
+            <h1 className="heading header__heading">
+                FFXIV Sightseeing Log Tracker
+            </h1>
+        </nav>
+    )
+}
+
+export default Header;

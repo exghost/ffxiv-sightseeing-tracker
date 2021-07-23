6ab52abd6141abca40340cbb04af3968dd4d839c
exghost <robertlucas198@gmail.com>
2021-07-22 23:19:53 -0400

    setup api

--- src/api/sightseeingLogs.ts
+++ src/api/sightseeingLogs.ts
@@ -1,5 +1,42 @@
+import EorzeaTime from 'eorzea-time';
+import EorzeaWeather from 'eorzea-weather';
+
 import data from 'db/sightseeing_data';
 
-export function getAllLogs(): any {
-    return data;
+
+export function getAllLogs(): any[] {
+    return data.logs;
+}
+
+export function getCurrentlyAvailableLogs(): any[] {
+    const time = new EorzeaTime();
+    const hour = time.getHours();
+    const logs = data.logs;
+
+    const currentLogsByTime = logs.filter(({ startTime, endTime }) => {
+        if(endTime > startTime) {
+            return hour >= startTime && hour < endTime;
+        } else {
+            return hour >= startTime || hour < endTime;
+        }
+    });
+
+    const currentLogsByTimeAndWeather = currentLogsByTime.filter(({ weatherZone, weather }) => {
+        let currentWeather = EorzeaWeather.getWeather(weatherZone, new Date());
+        console.log(weatherZone, currentWeather);
+
+        return weather.includes(currentWeather);
+    });
+
+    return currentLogsByTimeAndWeather;
+}
+
+export function getUniqueTimes(): any[] {
+    const logs = data.logs;
+
+    const startTimes = logs.map((log) => log.startTime);
+    const endTimes = logs.map((log) => log.endTime);
+
+    const uniqueTimes = new Set([...startTimes, ...endTimes]);
+    return Array.from(uniqueTimes);
 }

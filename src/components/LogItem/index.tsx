913354559a346a63eba53ed52664caf54345c82e
exghost <robertlucas198@gmail.com>
2021-07-25 11:32:41 -0400

    update content styling

--- src/components/LogItem/index.tsx
+++ src/components/LogItem/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 import './styles.css';
 
+import { getWeatherImg } from 'api/weatherImgs';
+
 function LogItem({ data }: { data: any }) {
     const [active, setActive] = useState(false);
 
@@ -21,11 +23,63 @@ function LogItem({ data }: { data: any }) {
 
             </div>
             { active &&
-                <ul>
-                    <li>Emote: {data.emote}</li>
-                    <li>Time: {`${data.startTime.toString()} - ${data.endTime.toString()}`} </li>
-                    <li>Weather: {data.weather.join(', ')}</li>
-                    <li>Notes: {data.comment}</li>
+                <ul className="log-item__body">
+                    <li
+                     className="log-item__body--emote log-item__body--item"
+                    >
+                        <span className="log-item__body--label log-item__body__emote--label"> 
+                            Emote:
+                        </span>
+                        <span className="log-item__body--content log-item__body__emote--content">
+                            {data.emote}
+                        </span>
+                    </li>
+                    <li
+                     className="log-item__body--time log-item__body--item"
+                    >
+                        <span className="log-item__body--label log-item__body__time--label"> 
+                            Time:
+                        </span>
+                        <span className="log-item__body--content log-item__body__time--content">
+                            {`${data.startTime.toString()}:00 - ${data.endTime.toString()}:00`}
+                        </span>
+                    </li>
+                    <li
+                     className="log-item__body--weather log-item__body--item"
+                    >
+                        <span className="log-item__body--label log-item__body__weather--label"> 
+                            Weather:
+                        </span>
+                        <span className="log-item__body--content log-item__body__weather--content">
+                            {
+                                data.weather.map((weather: string): JSX.Element => {
+                                    let weatherImg = getWeatherImg(weather);
+                                    if(weatherImg) {
+                                        return (
+                                            <img
+                                                key={weather}
+                                                src={weatherImg}
+                                                className="log-item__body--weather-image"
+                                                alt={weather}
+                                            />
+                                        );
+                                    } else {
+                                        return <></>;
+                                    }
+                                })
+                            }
+                        </span>
+                    </li>
+                    <li
+                     className="log-item__body--notes log-item__body--item"
+                    >
+                        <span className="log-item__body--label log-item__body__notes--label"> 
+                            Notes:
+                        </span>
+                        <span className="log-item__body--content log-item__body__notes--content">
+                            {data.comment}
+                        </span>
+                    </li>
                 </ul>
             }
         </div>

fb02931665d32aa487596e86de72494d7be788a9
exghost <robertlucas198@gmail.com>
2021-07-27 09:13:41 -0400

    general cleanup

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,17 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { getAllLogs } from 'api/sightseeingLogs';
 import CurrentActiveList from 'components/CurrentActiveList';
 
 function Home() {
-    useEffect(() => {
-        const getAll = async() => {
-            let logs = await getAllLogs();
-            console.log(logs);
-        }
-        getAll();
-    }, []);
-
     return (
         <div>
             <CurrentActiveList />
